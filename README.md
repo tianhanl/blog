b060625b8fe668a12576bab583f3d1cf4ee597a7
Tianhang Liu <tianhanl@usc.edu>
2017-08-02 14:22:38 +0800

    create router.js for routing componenets using vue-router and put it into main.js

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # blog
-A blog built with Vue.js
+
+> A blog built with Vue.js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+## Demo
+[Live Demo](http://tianhanl.github.io/blog);
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Router from "vue-router";
+import ListView from "./components/list-view.vue";
+import ArticleView from "./components/article-view.vue";
+
+// Use vue-router plugin
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "list",
+      component: ListView
+    },
+    {
+      path: "./article/:id",
+      name: "article",
+      component: ArticleView
+    }
+  ]
+});
+
+export default router;
