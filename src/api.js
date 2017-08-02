1decb5a8c53f8815880607f262cd8689195bdc05
Tianhang Liu <tianhanl@usc.edu>
2017-08-02 14:57:05 +0800

    add getArticles fucntion in list-view compoenet and modified the errors in api.js

--- src/api.js
+++ src/api.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 const getArticleList = function() {
   return axios.get(
-    'api.github.com/repos/tianhanl/tianhanl.github.io/issues?creator=tianhanl'
+    'https://api.github.com/repos/tianhanl/blog/issues?creator=tianhanl'
   );
 };
 
 const getArticle = function(id) {
-  return axios.get(
-    `https://api.github.com/repos/tianhanl/tianhanl.github.io/issues/${id}`
-  );
+  return axios.get(`https://api.github.com/repos/tianhanl/blog/issues/${id}`);
+};
+
+export default {
+  getArticleList,
+  getArticle
 };
--- src/components/list-view.vue
+++ src/components/list-view.vue
@@ -3,17 +3,40 @@
     <h2>Article List</h2>
     <ul>
       <li class="list-view-item" v-for="article in articles" :key="article.id">
-        <router-link :to="{name: 'article', params:{id:article.id}}">{{article.title}}</router-link>
+        <router-link :to="{name: 'article', params:{id:article.number}}">{{article.title}}</router-link>
       </li>
     </ul>
   </div>
 </template>
 <script>
+import API from '../api';
+
 export default {
   name: 'list-view',
   data() {
     return {
       articles: []
+    }
+  },
+  created() {
+    this.getArticles();
+  },
+  methods: {
+    getArticles() {
+      API.getArticleList()
+        .then(response => {
+          let list = response.data.map(element => {
+            return {
+              number: element.number,
+              id: element.id,
+              title: element.title
+            }
+          })
+          this.articles = list;
+        })
+        .catch(error => {
+          return;
+        });
     }
   }
 }
