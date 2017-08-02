350173bbd23fcb14cf830cb8d6b9d7af15245e7e
Tianhang Liu <tianhanl@usc.edu>
2017-08-02 14:12:10 +0800

    initial set up of this project

---
+++ src/components/article-view.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="article-view">
+    <div class="article-close">
+      <router-link to="/">
+        Back
+      </router-link>
+    </div>
+    <h2 class="article-headline"></h2>
+    <p class="article-meta"></p>
+    <div class="article-content" v-html="story">
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: 'article',
+  data: function () {
+    return {
+      story: '<h2>test</h2>'
+    }
+  },
+  created: function () {
+    this.requestArticle(this.$route.params.id);
+  },
+  watch: {
+    '$route'(to, from) {
+      this.requestArticle(this.$route.params.id);
+    }
+  },
+  methods: {
+    requestArticle: function (id) {
+      console.log(id);
+      let self = this;
+      // axios.get('/api/story'+id)
+      // .then(function(story){
+      //   self.story = story;
+      // })
+      // .catch(console.log(error));
+      self.story = '<h2>I am some stories</h2>' + id;
+    }
+  }
+}
+</script>
---
+++ src/components/list-view.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list-view">
+    <h2>Article List</h2>
+    <ul>
+      <li class="list-view-item" v-for="article in articles" :key="article.id">
+        <router-link :to="{name: 'article', params:{id:article.id}}">{{article.title}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'list-view',
+  data() {
+    return {
+      articles: []
+    }
+  }
+}
+</script>
+
