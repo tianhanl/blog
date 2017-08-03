003d006793040567c76d92e035911c8143b0f950
Tianhang Liu <tianhanl@usc.edu>
2017-08-03 12:35:17 +0800

    tweak the space between list-item

--- src/components/list-view.vue
+++ src/components/list-view.vue
@@ -57,7 +57,8 @@ export default {
 .list-view-item {
   border-bottom: 1px solid #A9B6C5;
   display: block;
-  padding: 1rem 0 1.2rem 0;
+  padding: 1rem 0 1.5rem 0;
+  margin-bottom: 1em;
   display: flex;
 }
 
