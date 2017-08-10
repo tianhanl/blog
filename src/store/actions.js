88165dd66318d58a0373dfc28b397687c0b3897a
Tianhang Liu <tianhanl@usc.edu>
2017-08-10 20:32:27 +0800

    add basic set up for vuex

---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+import api from '../api.js';
+import Vue from 'vue';
+import router from '../router';
+
+const actions = {};
+
+export default actions;
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import mutations from './mutations.js';
+import actions from './actions.js';
+
+Vue.use(Vuex);
+
+const state = {};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+});
