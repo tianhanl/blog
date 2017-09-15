f6f51f436095d195771e54a6c51e0806f7304f59
Tianhang Liu <tianhanl@usc.edu>
2017-09-14 22:19:17 -0700

    add page spliting and change page title dynamically

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import config from './config';
 import ListView from './components/list-view.vue';
-import ArticleView from './components/article-view.vue';
+const ArticleView = () => import('./components/article-view.vue');
 
 // Use vue-router plugin
 Vue.use(Router);
@@ -11,14 +12,21 @@ const router = new Router({
     {
       path: '/',
       name: 'list',
-      component: ListView
+      component: ListView,
+      meta: { title: config.blogTitle }
     },
     {
       path: '/article/:id',
       name: 'article',
-      component: ArticleView
+      component: ArticleView,
+      meta: { title: 'Article' }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
